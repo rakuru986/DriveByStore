7f75c5ce70aa6b6c39e054e297adb611b6c97082
Artur Kurvits <arkurv@ttu.ee>
2020-10-28 08:30:50 +0200

    correctly get products from controller

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -1,4 +1,8 @@
-﻿using Models.Context;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models.Context;
 using Models.Data;
 using Models.Store;
 using Models.Store.Interfaces;
@@ -8,9 +12,19 @@ namespace Repositories
 {
     public sealed class ProductRepository : UniqueEntityRepository<Product, ProductData>, IProductRepository
     {
+        private StoreDbContext db;
         public ProductRepository() : this(null) { }
-        public ProductRepository(StoreDbContext c = null) : base(c, c?.Products) { }
+
+        public ProductRepository(StoreDbContext c = null) : base(c, c?.Products)
+        {
+            db = c;
+        }
 
         protected internal override Product toModelObject(ProductData d) => new Product(d);
+
+        protected internal override IQueryable<ProductData> createSqlQuery()
+        {
+            return db.Products.Include(c => c.ProductCategory);
+        }
     }
 }
---
+++ Soft/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Maps;
+using Microsoft.AspNetCore.Mvc;
+using Models.Data;
+using Models.Store;
+using Models.Store.Interfaces;
+using ViewModels;
+
+namespace Soft.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository p)
+        {
+            productRepository = p;
+        }
+
+        // [Route("api/products")]
+        // product/GetAllProducts
+        public JsonResult GetAllProducts()
+        {
+            var products = productRepository.Get().Result;
+            return Json(products);
+        }
+
+        public JsonResult SaveProduct(ProductViewModel product)
+        {
+            try
+            {
+                ProductsMapper mapper = new ProductsMapper();
+                Product productItem = mapper.mapProducts(product);
+                return Json("Successful!");
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message);
+            }
+
+        }
+    }
+}
