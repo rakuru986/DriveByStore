d83247af07efc5660674cc8adfd4cb8e0625e578
Rasmus Kurušev <rakuru@ttu.ee>
2020-11-15 18:05:43 +0200

    ProductCategory & ProductRepository testid alugus

---
+++ Tests/Repositories/Products/ProductRepositoryTests.cs
@@ -0,0 +1,42 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using Models.Common.Interfaces;
+//using Util.Random;
+
+//namespace Tests.Repositories.Products {
+
+//    public abstract class
+//        ProductRepositoryTests<TRepository, TDomain, TData> : SealedTests<TRepository,
+//            PaginatedRepository<TDomain, TData>>
+//        where TRepository : PaginatedRepository<TDomain, TData>
+//        where TData : PeriodData, new()
+//        where TDomain : IEntity<TData> {
+
+//        protected ProductDbContext db;
+
+//        [TestInitialize] public override void TestInitialize() {
+//            base.TestInitialize();
+//            var options = new DbContextOptionsBuilder<ProductDbContext>().UseInMemoryDatabase("TestDb").Options;
+//            db = new ProductDbContext(options);
+//        }
+
+//        [TestMethod] public void CanSetContextAndSetTest() {
+//            obj = getObject(db);
+//            Assert.AreSame(db, obj.db);
+//            Assert.AreSame(getSet(db), obj.dbSet);
+//        }
+
+//        protected abstract TRepository getObject(ProductDbContext db);
+
+//        protected abstract DbSet<TData> getSet(ProductDbContext db);
+
+//        [TestMethod] public virtual void ToDomainObjectTest() {
+//            var d = (TData) GetRandom.Object(typeof(TData));
+//            var o = obj.toDomainObject(d);
+//            arePropertiesEqual(d, o.Data);
+//        }
+
+//    }
+
+//}
+
