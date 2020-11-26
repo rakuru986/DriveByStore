e85c51d2356690054b5b826001f1402075bb8ef9
Rasmus Kurušev <rakuru@ttu.ee>
2020-11-26 18:20:17 +0200

    OrderTests, ProducCategoryTests, ProducTests

---
+++ Tests/Models/Store/ProductCategoryTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models.Common;
+using Models.Data.Common;
+using Models.Data.Products;
+using Models.Store;
+using Util.Random;
+
+namespace Tests.Models.Store
+{
+
+    [TestClass]
+    public class ProductCategoryTests : SealedTests<ProductCategory, UniqueEntity<ProductCategoriesData>>
+    {
+
+        protected override ProductCategory createObject() =>
+            new ProductCategory(GetRandom.Object<ProductCategoriesData>());
+
+    }
+}
