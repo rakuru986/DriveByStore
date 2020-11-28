6000d40d58644b2a706192d17ea383fb39261f31
Kristen Niilop <kristen.niilop@gmail.com>
2020-11-28 22:12:11 +0200

    qty chekc added

--- Soft/ClientApp/src/app/shopping-cart/product-list/product-item/product-item.component.ts
+++ Soft/ClientApp/src/app/shopping-cart/product-list/product-item/product-item.component.ts
@@ -1,3 +1,4 @@
+import { isPlatformWorkerApp } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
 import { MessengerService } from 'src/app/services/messenger.service'
@@ -15,8 +16,18 @@ export class ProductItemComponent implements OnInit{
 
     ngOnInit() {}
 
-    handleAddToCart(){
-        this.msg.sendMsg(this.productItem)
+    handleAddToCart(){        
+        if(this.productItem.stock > 0){
+            this.msg.sendMsg(this.productItem)
+            // this.productItem.stock -= 1;
+        }
+        else
+        {
+            console.log("Pole piisavalt tooteid")
+        }
     }
+
+    
+    
 
 }
